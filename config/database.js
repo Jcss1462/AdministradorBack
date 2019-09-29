2e2c6c0d03d793fa83a68fbe48840d0b6a5ec92d
jcss1462 <jcss1462@gmail.com>
2019-09-28 23:33:53 -0500

    login prfecto

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   'gestor': {
-    user: process.env.NODE_ORACLEDB_USER || "system",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "oracle",
+    user: process.env.NODE_ORACLEDB_USER || "jcss1462",
+    password: process.env.NODE_ORACLEDB_PASSWORD || "jcss1462",
     connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/xe",
 
     externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false,
--- db_apis/login.js
+++ db_apis/login.js
@@ -2,63 +2,80 @@
 const database = require('../services/database.js');
 const oracledb = require('oracledb');
 
+
 const baseQuery =
-  `select id_usuario "id_usuario",
+  `select 
+    id_usuario "id_usuario",
     nombre "nombre",
     username "username",
     id_tipo "id_tipo"
-  from usuario`;
-
+    from usuario`;
 
 
 async function find(context) {
   //console.log(488888);
-  let query = baseQuery;
-  const binds = {};
+
+  let fakeresult= {};
+
+  if (context.username && context.password) {
 
 
-  if (context.username&&context.password) {
-    //coloco el id del contexto en el bind
-    binds.username = context.username;
-    binds.password = context.password;
 
-    query += `\nwhere username = :username and contraseña = :password`;
+    let auto = await autenticacion(context.username, context.password);
+
+    let prueba = Number(auto.val);
+    console.log(prueba);
+
+
+    if (prueba != 0) {
+      let permiso = auto.val;
+      console.log("permiso= " + permiso);
+
+      console.log("empiezo la extraccion de datos");
+
+      let query;
+      const binds = {};
+      binds.id_usuario = permiso;
+
+      console.log("¿problemas?");
+
+      query = baseQuery + `\nwhere id_usuario = :id_usuario`;
+
+      console.log("query= " + query);
+
+      let result = await database.simpleExecute(query, binds);
+
+      console.log('Extraccion de datos finalizada');
+      return result.rows;
+
+
+
+    } else {
+
+      console.log("error");
+      
+      return fakeresult;
+    }
+  }else{
+
+    return fakeresult;
 
   }
 
-
-
-
-  console.log(query);
-
-  /////////////////////////////////////////////////////
-
-
-  //////////////////////////////////restriccines de paginacion
-
-
-  if (context.skip) {
-    binds.row_offset = context.skip;
-
-    query += '\noffset :row_offset rows';
-  }
-
-  const limit = (context.limit > 0) ? context.limit : 30;
-
-  binds.row_limit = limit;
-
-  query += '\nfetch next :row_limit rows only';
-
-  //////////////////////////////////////////////////////////////////
-
-  let result = await database.simpleExecute(query, binds);
-
-
-  //añado la imagen a la consulta
-  console.log(result);
-  return result.rows;
 }
 
 module.exports.find = find;
 
 
+const verificacion = `BEGIN :val := login(:user,:pass); END;`;
+
+async function autenticacion(username, password) {
+  console.log('autenticacion empezo');
+  const binds = {};
+  binds.user = username;
+  binds.pass = password;
+  binds.val = { dir: oracledb.BIND_OUT, type: oracledb.STRING, maxSize: 500000 };
+  const result = await database.simpleExecute(verificacion, binds);
+  console.log('autenticacion termino');
+  return result.outBinds;
+}
